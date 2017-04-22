9df901671fde3e7203de589f8fa1bc5cddac3f68
nikhil <nikhil4596@gmail.com>
2017-04-22 17:36:40 -0400

    Getting There

---
+++ js/index2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nikhil on 4/22/2017.
+ */
+$('#register').click(function(){
+    window.location.href="main.html";
+});
+$('#login').click(function(){
+    window.location.href="main.html";
+});
