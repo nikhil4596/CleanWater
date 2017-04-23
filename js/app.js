04806fce71296430304c879c8989dd4c17033586
nikhil <nikhil4596@gmail.com>
2017-04-23 04:18:57 -0400

    Implemented Purity and Source Report and most of the functionality

---
+++ js/app.js
@@ -0,0 +1,108 @@
+/**
+ * Created by nikhil on 4/23/2017.
+ */
+var currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+var users = JSON.parse(sessionStorage.getItem('users'));
+var reports = JSON.parse(sessionStorage.getItem('reports'));
+if (reports === null) {
+    reports = [];
+}
+
+$(document).ready(function() {
+    if (currentUser.userRole === "User") {
+        document.getElementById('purityReport').style.display = 'none';
+        document.getElementById('historicalReport').style.display = 'none';
+    }
+    if (currentUser.userRole === "Worker") {
+        document.getElementById('historicalReport').style.display = 'none';
+    }
+});
+function SourceReport(email, waterType, waterCondition, latitude, longitude) {
+    this.authorEmail = email;
+    this.timeS = new Date();
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.waterType = waterType;
+    this.waterCondition = waterCondition;
+}
+function PurityReport(email, waterCondition, virus, contaminant, latitude, longitude) {
+    this.authorEmail = email;
+    this.timeS = new Date();
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.waterCondition = waterCondition;
+    this.virus = virus;
+    this.contaminant = contaminant;
+}
+$(document).ready(function () {
+    document.getElementById('descrp').innerHTML += currentUser.name;
+});
+
+$('#editProfileSubmit').click(function () {
+    currentUser.name = document.getElementById('editName').value;
+    currentUser.address = document.getElementById('editAddress').value;
+    console.log(currentUser);
+    removeCurrentUser();
+    // sessionStorage.removeItem('currentUser');
+    // sessionStorage.removeItem('users');
+    users.push(currentUser);
+    sessionStorage.setItem('currentUser',JSON.stringify(currentUser));
+    sessionStorage.setItem('users',JSON.stringify(users));
+
+
+});
+
+$('#reportsList').click(function () {
+    document.getElementById('listOfReports').innerHTML = "";
+    reports.forEach(function (item) {
+        document.getElementById('listOfReports').innerHTML += "Created by: " + item.authorEmail + " on " + item.timeS + " at " + item.latitude + "," + item.longitude+ " </br>";
+    })
+})
+
+$('#createSourceReport').click(function () {
+    var waterType = document.getElementById('waterType').value;
+    var waterCondition = document.getElementById('waterCondition').value;
+    var lat = Number(document.getElementById('lat1').value);
+    var long = Number(document.getElementById('long1').value);
+    var report = new SourceReport(currentUser.email, waterType, waterCondition, lat, long);
+    console.log(report);
+    reports.push(report);
+    sessionStorage.setItem('reports',JSON.stringify(reports));
+    $('#myModalSource').modal('hide');
+    window.location = window.location.href;
+    console.log("Refreshed?")
+
+});
+$('#createPurityReport').click(function () {
+    var waterCondition = document.getElementById('waterCondition2').value;
+    var lat = Number(document.getElementById('lat2').value);
+    var long = Number(document.getElementById('long2').value);
+    var virus = Number(document.getElementById('virusPPM').value);
+    var contaminant = Number(document.getElementById('contaminantPPM').value);
+    var report = new PurityReport(currentUser.email, waterCondition, virus, contaminant, lat, long);
+    console.log(report);
+    reports.push(report);
+    sessionStorage.setItem('reports',JSON.stringify(reports));
+    $('#myModalPurity').modal('hide');
+    window.location.href = window.location.href;
+    console.log("Refreshed?")
+});
+
+function removeCurrentUser() {
+    users.forEach(function (item) {
+        if (item.email === currentUser.email) {
+            var index = users.indexOf(item);
+            users.splice(index, 1);
+        }
+    })
+}
+$('#logOut').click(function () {
+    console.log("logged you out bithc");
+    currentUser = null;
+    sessionStorage.setItem('currentUser',JSON.stringify(null));
+    window.location.href = 'index.html';
+});
+
+
+console.log(reports);
+console.log(JSON.parse(sessionStorage.getItem('currentUser')));
--- js/index.js
+++ js/index.js
@@ -1,9 +1,128 @@
 /**
  * Created by nikhil on 4/22/2017.
  */
+var txtsignInEmail = document.getElementById('emailAddress');
+var txtregisterEmail = document.getElementById('emailAddress2');
+var txtsignInPassword = document.getElementById('password');
+var txtregisterPassword = document.getElementById('password2');
+var txtregisterPassword2 = document.getElementById('password3');
+var txtregisterRole = document.getElementById('role');
+var btnsignin = document.getElementById('login');
+var btnregister = document.getElementById('register');
+var currentUser;
+var reports = JSON.parse(sessionStorage.getItem('reports'));
+if (reports === null) {
+    reports = [];
+    sessionStorage.setItem('reports', JSON.stringify(reports));
+}
+var users = JSON.parse(sessionStorage.getItem('users'));
+if (users === null) {
+    users = [];
+}
+
+function User(email, password, role, name, address) {
+    this.email = email;
+    this.pass = password;
+    this.userRole = role;
+    this.name = name;
+    this.address = address;
+}
+
+function verifyPass() {
+    if (txtregisterPassword.value === txtregisterPassword2.value) {
+        return true;
+    } else {
+        document.getElementById('retypePass').innerHTML += " - Passwords do not match!";
+        document.getElementById('retypePass').focus();
+    }
+}
+
 $('#register').click(function(){
-    window.location.href="main.html";
+    if (verifyPass()) {
+        var email = txtregisterEmail.value;
+        var pass = txtregisterPassword.value;
+        var role = txtregisterRole.value;
+        var novel = true;
+        users.forEach(function (user) {
+            if (user.email === email) {
+                novel = false;
+            }
+        });
+        if (novel) {
+            currentUser = new User(email, pass, role, email, 'N/A');
+            users.push(currentUser);
+            console.log("Created you" + currentUser.userRole);
+            sessionStorage.setItem('users', JSON.stringify(users));
+            sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+            window.location.href = "main.html";
+        } else {
+            console.log("User already exists");
+        }
+    } else {
+        console.log("Passwords do not match!");
+    }
 });
+//
+// $('#register').click(function(){
+//     if (verifyPass()) {
+//         const email = txtregisterEmail.value;
+//         const pass = txtregisterPassword.value;
+//         const auth = firebase.auth();
+//         const promise = auth.createUserWithEmailAndPassword(email, pass);
+//         promise.catch(function (e) {
+//             console.log(e.message);
+//             return;
+//         });
+//         console.log("Created you");
+//         // window.location.href = "main.html";
+//     } else {
+//         console.log("Passwords do not match!");
+//     }
+// });
 $('#login').click(function(){
-    window.location.href="main.html";
+    var email = txtsignInEmail.value;
+    var pass = txtsignInPassword.value;
+    var role = txtregisterRole.value;
+    var auth = false;
+    users.forEach(function (user) {
+        if (user.email === email && user.pass == pass) {
+            auth = true;
+        }
+    });
+    if (auth) {
+        currentUser = new User(email, pass, role, email, 'N/A');
+        users.push(currentUser);
+        console.log("Logged you in!" + currentUser);
+        sessionStorage.setItem('users',JSON.stringify(users));
+        sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+        window.location.href = "main.html";
+    } else {
+        console.log('User doesnot exist');
+    }
 });
+// $('#login').click(function(){
+//     const email = txtsignInEmail.value;
+//     const pass = txtsignInPassword.value;
+//     const auth = firebase.auth();
+//     const promise = auth.signInWithEmailAndPassword(email,pass);
+//     promise.catch(function(e) {
+//         console.log(e.message);
+//         return;
+//     });
+//     console.log("Logged you in!");
+//     window.location.href="main.html";
+// });
+// $('#logOut').click(function () {
+//     firebase.auth().signOut();
+//     window.location.href='index.html'
+// });
+
+// firebase.auth().onAuthStateChanged(firebaseUser =>  {
+//     if (firebaseUser) {
+//         console.log(firebaseUser);
+//         // newUser = new User()
+//     } else {
+//         console.log("Not Logged In")
+//     }
+// });
+
