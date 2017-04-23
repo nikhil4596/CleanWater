4df1141c86fbf7103ef96f94988259bc0d71da9f
nikhil <nikhil4596@gmail.com>
2017-04-22 21:38:48 -0400

    Made Most of the UI

--- js/index2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by nikhil on 4/22/2017.
- */
-$('#register').click(function(){
-    window.location.href="main.html";
-});
-$('#login').click(function(){
-    window.location.href="main.html";
-});
